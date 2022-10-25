2edad1f0bf602f0552267ea641e6c4970622b993
Anouar-91 <a.berrouane@hotmail.com>
2022-10-25 17:53:17 +0200

    add fetch function

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import products from '../products';
 import Product from '../components/Product';
+import axios from 'axios';
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState();
+    const [loading, setLoading] = useState(true)
+
+    const fetchProducts = async() => {
+        const {data} = await axios.get('http://localhost:3000/api/products');
+        setProducts(data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    },[])
+
     return (
         <>
             <h1>Latest Products</h1>
+            {loading ? 'chargement...' : 
             <div className="row">
-                {products.map(product => {
-                    return (
-                        <div key={product._id} className="col-md-4">
-                           <Product product={product} />
-                        </div>
-                    )
+            {products.map(product => {
+                return (
+                    <div key={product._id} className="col-md-4">
+                       <Product product={product} />
+                    </div>
+                )
 
-                })}
-            </div>
+            })}
+        </div>
+
+    }
+
         </>
     )
 }

a0057a0d7720c5d873780cbf1307ee40a11e5cec
Anouar-91 <a.berrouane@hotmail.com>
2022-11-02 14:59:36 +0100

    add action redux to retrieve data products

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,38 +1,52 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import products from '../products';
 import Product from '../components/Product';
 import axios from 'axios';
+//redux
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../redux/actions/productActions';
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState();
-    const [loading, setLoading] = useState(true)
+    //without redux
+    /*  const [products, setProducts] = useState();
+        const [loading, setLoading] = useState(true)
+    
+        const fetchProducts = async() => {
+            const {data} = await axios.get('http://localhost:3000/api/products');
+            setProducts(data);
+            setLoading(false);
+        }
+        useEffect(() => {
+            fetchProducts()
+        },[]) */
 
-    const fetchProducts = async() => {
-        const {data} = await axios.get('http://localhost:3000/api/products');
-        setProducts(data);
-        setLoading(false);
-    }
+    //with redux
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        fetchProducts()
-    },[])
+        dispatch(listProducts())
+    }, [dispatch])
+
+
 
     return (
         <>
             <h1>Latest Products</h1>
-            {loading ? 'chargement...' : 
-            <div className="row">
-            {products.map(product => {
-                return (
-                    <div key={product._id} className="col-md-4">
-                       <Product product={product} />
-                    </div>
-                )
+            {loading ? 'chargement...' :
+                <div className="row">
+                    {products.map(product => {
+                        return (
+                            <div key={product._id} className="col-md-4">
+                                <Product product={product} />
+                            </div>
+                        )
 
-            })}
-        </div>
+                    })}
+                </div>
 
-    }
+            }
 
         </>
     )

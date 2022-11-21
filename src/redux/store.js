49aa37ea009bfc90a2063982aae4e474dd71e8f6
Anouar-91 <a.berrouane@hotmail.com>
2022-11-21 15:09:16 +0100

    manage cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,19 @@
 import { createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import {productListReducer, productDetailReducer} from './reducers/productReducer'
+import {productListReducer, productDetailReducer} from './reducers/productReducer';
+import {cartReducer } from '../redux/reducers/cartReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetail: productDetailReducer
+    productDetail: productDetailReducer,
+    cart: cartReducer
 })
 
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
 const initialState = {
+    cart: {cartItems: cartItemsFromStorage}
 }
 const middleware  = [thunk]
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));

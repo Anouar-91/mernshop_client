1aad6a4a634a6c5ead447bf7e2fff88990d3838c
Anouar-91 <a.berrouane@hotmail.com>
2023-01-18 21:10:50 +0100

    add screen user edit

--- src/screens/UserListScreen.jsx
+++ src/screens/UserListScreen.jsx
@@ -24,7 +24,7 @@ const UserListScreen = () => {
         }else{
             navigate('/login')
         }
-    }, [dispatch, successDelete ])
+    }, [dispatch, successDelete, userInfo ])
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure ?')){
@@ -55,7 +55,7 @@ const UserListScreen = () => {
                                 <td> <a href={`mailot:${user.email}`}>{user.email}</a></td>
                                 <td>{user.isAdmin ? <i class="fa-solid fa-check text-success"></i> : <i class="fa-solid fa-xmark text-danger"></i>}</td>
                                 <td>
-                                    <Link  className="btn btn-sm btn-warning" to={`/user/${user._id}/edit`}><i class="fa-regular fa-pen-to-square"></i></Link> 
+                                    <Link  className="btn btn-sm btn-warning" to={`/admin/user/${user._id}/edit`}><i class="fa-regular fa-pen-to-square"></i></Link> 
                                     <button className="btn btn-sm btn-danger" onClick={() => deleteHandler(user._id)}><i class="fa-solid fa-trash"></i></button>
                                 </td>
                             </tr>

a049b07cb2d8a47bbe00fd8479e1c801a8b8cecd
Anouar-91 <a.berrouane@hotmail.com>
2023-02-15 11:46:18 +0100

    paginate in homePage

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,43 +1,47 @@
 import React, { useEffect, useState } from 'react'
 import Product from '../components/Product';
-import {ThreeDots} from  'react-loader-spinner'
+import { ThreeDots } from 'react-loader-spinner'
 //redux
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../redux/actions/productActions';
 import Message from '../components/Message';
 import { useParams } from 'react-router-dom';
+import Paginate from '../components/Paginate';
 
 
 const HomeScreen = () => {
-    let {keyword} = useParams();
+    let { keyword } = useParams();
+    let { pageNumber } = useParams();
     const dispatch = useDispatch();
     const productList = useSelector(state => state.productList)
-    const { loading, error, products} = productList
+    const { loading, error, products, page, pages } = productList
 
     useEffect(() => {
-        dispatch(listProducts(keyword))
-    }, [dispatch, keyword])
-
-
+        dispatch(listProducts(keyword, pageNumber))
+    }, [dispatch, keyword, pageNumber])
 
     return (
         <div className="container">
             <h1>Latest Products</h1>
-            {loading 
-            ? <ThreeDots wrapperStyle={{justifyContent: 'center'}} /> 
-            : error 
-            ? (<Message variant="danger">{error}</Message>) :
-                <div className="row">
-                    {products.map(product => {
-                        return (
-                            <div key={product._id} className="col-md-4">
-                                <Product product={product} />
-                            </div>
-                        )
+            {loading
+                ? <ThreeDots wrapperStyle={{ justifyContent: 'center' }} />
+                : error
+                    ? (<Message variant="danger">{error}</Message>) :
+                    <>
+                        <div className="row">
+                            {products.map(product => {
+                                return (
+                                    <div key={product._id} className="col-md-4">
+                                        <Product product={product} />
+                                    </div>
+                                )
 
-                    })}
-                </div>
-
+                            })}
+                        </div>
+                        <div className="mt-5">
+                            <Paginate pages={pages} page={page} keyword={keyword ? keyword : ""} />
+                        </div>
+                    </>
             }
 
         </div>

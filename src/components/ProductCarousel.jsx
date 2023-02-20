79aeb0141b99cb91264f6f304560a4cb2d14c260
Anouar-91 <a.berrouane@hotmail.com>
2023-02-20 23:36:07 +0100

    replace class by className

--- src/components/ProductCarousel.jsx
+++ src/components/ProductCarousel.jsx
@@ -17,18 +17,18 @@ const ProductCarousel = () => {
 
     return (
         loading ? <Loader /> : error ? <Message variant={"danger"}>{error}</Message> : (
-            <div id="carouselExampleCaptions" class="carousel slide">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <div id="carouselExampleCaptions" className="carousel slide">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-inner">
+                <div className="carousel-inner">
                     {products.map((product, key) => {
                         return (
                         <div class={key == 0 ? "active carousel-item" : "carousel-item"}>
-                            <img src={product.image} class="d-block" alt="..." />
-                            <div class="carousel-caption d-none d-md-block">
+                            <img src={product.image} className="d-block" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
                                 <h2>{product.name}
                                 </h2>
                             </div>
@@ -39,13 +39,13 @@ const ProductCarousel = () => {
 
 
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
                 </button>
             </div>
         )
